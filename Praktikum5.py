92b93bcbbafb32ee015a0a7faf7cfb3ec1af4978
rizqimaulana04 <mrizqim01@gmail.com>
2022-11-21 20:46:50 +0700

    Upload File

---
+++ Praktikum5.py
@@ -0,0 +1,38 @@
+print('Nama \t : MUHAMMAD RIZQI MAULANA')
+print('kelas \t : TI.22.A.4')
+print('Prodi \t : TEKNIK INFORMATIKA')
+print(' ')
+i=0
+nm =[]
+ni = []
+tgs = []
+uts = []
+uas = []
+akh = []
+
+while True:
+    nama = input('Nama\t\t\t: ')
+    nm.append(nama)
+    nim = input('NIM\t\t\t: ')
+    ni.append(nim)
+    nilai_tugas = input('Nilai Tugas\t\t: ')
+    tgs.append(nilai_tugas)
+    nilai_uts = input('Nilai UTS\t\t: ')
+    uts.append(nilai_uts)
+    nilai_uas = input('Nilai UAS\t\t: ')
+    uas.append(nilai_uas)
+    akhir =(int(nilai_tugas) *0.3) + (int(nilai_uts) *0.35) + (int(nilai_uas) *0.35)
+    akh.append(akhir)
+
+    data = (' ')
+    while data!='y' and data!='t':
+        data = input('Tambah Data (y/t)?')
+    i+=1
+    if data =='t':
+        break
+print("==================================================================")
+print("| No |  Nama\t|\tNIM\t| TUGAS\t|  UTS\t|  UAS\t|  Akhir |")
+print("==================================================================")
+for n in range(i):
+    print("|",n+1," |",nm[n],"\t|",ni[n],"\t|", tgs[n],"\t|",uts[n],"\t|", uas[n],"\t|", akh[n]," |")
+print("==================================================================") 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Praktikum5
+### program menghitung dan menampilkan data mahasiswa dan rata rata nilai yang di dapat, dengan perhitungan nilai (tugas : 30%, uts 35%, uas 35%). yang di input pada program ini yaitu Nama, Nim, Nilai tugas , Nilai uts, Nilai uas, (nilai akhir dihitung dari perhitungan nilai). Hasil outputnya berupa table.
+### Saya juga menggunakan list untuk program input pada koding, yaitu menggunakan python list methods : append ( ) menambah item dari belakang, selain itu saya menggunakan while untuk mengulang pertanyaan.
+### Berikut contoh Gambar Sintaksnya
+![img](gambar/1.png)</br>
+![img](gambar/2.png) </br>
+### Berikut contoh Gambar Output
+![img](gambar/output.png)</br>
+<p>SELESAI</p>
